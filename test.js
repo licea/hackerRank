4ff6c04d04ab470999a271c814f3b5c439eeaacd
Erik <eriklicea@ciencias.unam.mx>
2023-03-21 18:17:17 -0600

    Area Calculation Function

--- test.js
+++ test.js
@@ -117,3 +117,52 @@ function timeConversion(s) {
     }
     return newHour + ':' + minutes + ':' + seconds
 }
+
+//Area Solver
+
+const getArea = (shape, values) => {
+    let area = -1
+    switch(shape) {
+        case "square":
+            area = Math.pow(values[0], 2);
+            break;
+        case "rectangle":
+            area = values[0] * values[1];
+            break;
+        case "circle":
+            area = 3.14 * Math.pow(values[0], 2);
+            break;
+        case "triangle":
+            area = 0.5 * values[0] * values[1];
+            break;
+        default:
+            break;
+    }
+    return area;
+}
+
+// Complete the calculateArea function below.
+// It returns a Promise which on success, returns area of the shape, and on failure returns [-1].
+let calculateArea = async (shape, values) => {
+    const area = getArea(shape, values)
+    if (area == -1) {
+        throw new Error(-1)
+    }
+    else{
+        return Math.floor(area * 100) / 100
+    } 
+}
+
+// Complete the generateArea function below.
+// It returns a Promise which on success, returns an array of areas of all the shapes and on failure, returns [-1].
+let getAreas = async (shapes, values_arr) => {
+    try {
+        const areas = await Promise.all(
+        shapes.map((shape, index) => calculateArea(shape, values_arr[index]))
+        )
+        return areas;
+    }
+    catch {
+        return [-1]
+    }
+}
