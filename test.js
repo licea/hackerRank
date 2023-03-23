95dd1b0042a74718ee055e09a9bb93d3d554aee3
Erik <eriklicea@ciencias.unam.mx>
2023-03-22 23:34:56 -0600

    Kangaroo Jumps Solution

--- test.js
+++ test.js
@@ -207,3 +207,16 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     }
     console.log(orange_counter)
 }
+
+//KANGAROO JUMPS
+
+function kangaroo(x1, v1, x2, v2) {
+    for(let i=0; i<10000; i++) {
+        x1 += v1
+        x2 += v2
+        if (x1 == x2) {
+            return 'YES'
+        }
+    }
+    return 'NO'
+}
