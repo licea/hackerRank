aef1b2dafbc56115619f6cfecdad757fefe0e3d2
Erik <eriklicea@ciencias.unam.mx>
2023-04-24 13:37:00 -0600

    Update betweenTwoSets.js
    Se agrega función para testing

--- betweenTwoSets.js
+++ betweenTwoSets.js
@@ -1,3 +1,6 @@
+let test1 = [2]
+let test2 = [20, 30, 12]
+
 function intersection(setA, setB) {
   let _intersection = new Set()
   for (let elem of setB) {
@@ -11,17 +14,17 @@ function intersection(setA, setB) {
 function getTotalX(a, b) {
     let x = a[a.length - 1]
     let y = b[0]
-    let divisoresTmp =[]
-    let divisores = new Set()
+    let divisoresTmpA =[]
+    let divisoresA = new Set()
     for (let i=x; i<=y; i++) {
         if (i % a[0] === 0) {
-            divisoresTmp.push(i)
+            divisoresTmpA.push(i)
         }
     }
     for (let i=x; i<=y; i++) {
         for (let j=1; j<a.length; j++) {
-            if (i % a[j] === 0 && divisoresTmp.includes(i)) {
-                divisores.add(i)
+            if (i % a[j] === 0 && divisoresTmpA.includes(i)) {
+                divisoresA.add(i)
                 //console.log(i)
             }
         }
@@ -42,12 +45,16 @@ function getTotalX(a, b) {
             }
         }
     }
-    const resultado = intersection(divisores, divisoresB)
+    const resultado = intersection(divisoresA, divisoresB)
     let counter = 0
     for (let item of resultado) {
         if (item >= x && item <= y) {
             counter++
+            console.log(item)
         }
     }
     return counter
 }
+
+
+console.log(getTotalX(test1, test2))
